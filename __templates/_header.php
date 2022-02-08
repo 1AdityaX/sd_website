2f53fdfd4105e1416bb924b834bf8e7fe2f0fb6f
AnonymousXsn <adityakb2005@gmail.com>
2022-02-08 16:11:24 +0530

    update

--- __templates/_header.php
+++ __templates/_header.php
@@ -1,6 +1,6 @@
 <main>
     <div class="container">
-        <header class="d-flex flex-wrap align-items-center justify-content-center justify-content-md-between py-3 mb-4 border-bottom">
+        <header class="d-flex flex-wrap align-items-center justify-content-center justify-content-md-between py-3 mb-4">
             <a href="/" class="d-flex align-items-center col-md-3 mb-2 mb-md-0 text-dark text-decoration-none">
 
             </a>
@@ -10,7 +10,6 @@
                 <li class="nav-item"><a href="#" class="nav-link">Credits</a></li>
                 <li class="nav-item"><a href="#" class="nav-link">Leaderboard</a></li>
                 <li class="nav-item"><a href="#" class="nav-link">Settings</a></li>
-                <!-- <li class="nav-item"><a href="#" class="nav-link">About</a></li> -->
             </ul>
 
             <div class="col-md-3 text-end">
