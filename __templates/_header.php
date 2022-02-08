300ff6b7f4bd048a34f86776c9cb639017714c54
ChaosXsniffer <biscuitbot07@gmail.com>
2022-02-08 23:39:37 +0530

    update

--- __templates/_header.php
+++ __templates/_header.php
@@ -1,22 +1,18 @@
-<main>
-    <div class="container">
-        <header class="d-flex flex-wrap align-items-center justify-content-center justify-content-md-between py-3 mb-4">
-            <a href="/" class="d-flex align-items-center col-md-3 mb-2 mb-md-0 text-dark text-decoration-none">
+<link href="../css/header.css", rel="stylesheet">
+<header class="d-flex flex-wrap align-items-center justify-content-center justify-content-md-between py-3 mb-4 border-bottom">
+    <a href="/" class="d-flex align-items-center col-md-3 mb-2 mb-md-0 text-dark text-decoration-none">
 
-            </a>
+    </a>
 
-            <ul class="nav nav-pills">
-                <li class="nav-item"><a href="#" class="nav-link active" aria-current="page">Home</a></li>
-                <li class="nav-item"><a href="#" class="nav-link">Credits</a></li>
-                <li class="nav-item"><a href="#" class="nav-link">Leaderboard</a></li>
-                <li class="nav-item"><a href="#" class="nav-link">Settings</a></li>
-            </ul>
+    <ul class="nav nav-pills">
+        <li class="nav-item"><a href="#" class="nav-link active" aria-current="page">Home</a></li>
+        <li class="nav-item"><a href="#" class="nav-link">Credits</a></li>
+        <li class="nav-item"><a href="#" class="nav-link">Leaderboard</a></li>
+        <li class="nav-item"><a href="#" class="nav-link">Settings</a></li>
+    </ul>
 
-            <div class="col-md-3 text-end">
-                <button type="button" class="btn btn-outline-primary me-2">Login</button>
-                <button type="button" class="btn btn-primary">Sign-up</button>
-            </div>
-        </header>
+    <div class="col-md-3 text-end">
+        <button type="button" class="btn btn-outline-primary me-2">Login</button>
+        <button type="button" class="btn btn-primary">Sign-up</button>
     </div>
-</main>
-<script src="../assets/dist/js/bootstrap.bundle.min.js"></script>
+</header>
